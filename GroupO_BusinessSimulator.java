bf6593986f384402cc3b1d0faf5af562caf32c98
eram-ok <erasmuscruise@gmail.com>
2026-08-28 17:37:41 +0300

    Created project and added arrays for items

--- GroupO_BusinessSimulator.java
+++ GroupO_BusinessSimulator.java
@@ -20,75 +20,12 @@ public class GroupO_BusinessSimulator {
 
         // Quantities being bought
         int[] quantities = {
-            19,
-            2,
-            2,
-            4
+            19,  // Petrol
+            2,   // Diesel
+            2,   // Engine Oil
+            4    // Car Wash
         };
 
-        // -----------------------------
-        // DISPLAY PRICE LIST
-        // -----------------------------
-
-        System.out.println("===== POWERFUEL STATION =====");
-
-        for (int i = 0; i < itemNames.length; i++) {
-
-            System.out.printf(
-                "%d. %s - UGX %.2f%n",
-                i + 1,
-                itemNames[i],
-                prices[i]
-            );
-        }
-
-        // -----------------------------
-        // CALCULATE SUBTOTALS
-        // -----------------------------
-
-        double[] subtotals = new double[itemNames.length];
-
-        for (int i = 0; i < itemNames.length; i++) {
-
-            subtotals[i] = calculateSubtotal(
-                prices[i],
-                quantities[i],
-                i
-            );
-        }
     }
 
-    public static double calculateSubtotal(
-        double price,
-        int quantity,
-        int itemIndex
-    ) {
-
-        double subtotal = price * quantity;
-
-        if (itemIndex == 0) {
-
-            if (quantity >= 20) {
-                subtotal = subtotal - (subtotal * 0.05);
-            }
-
-        } else if (itemIndex == 1) {
-
-            // Diesel has no discount
-
-        } else if (itemIndex == 2) {
-
-            if (quantity >= 3) {
-                subtotal = subtotal - 3000;
-            }
-
-        } else if (itemIndex == 3) {
-
-            if (quantity >= 4) {
-                subtotal = subtotal - (subtotal * 0.10);
-            }
-        }
-
-        return subtotal;
-    }
 }
